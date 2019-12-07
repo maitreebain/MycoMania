3d4dbd8fa7d4328daa1dcd9afd6049a9e8f18d3f
maitreebain <maitreebain@gmail.com>
2019-12-06 22:43:43 -0500

    getting data from json

--- ProjectMushroom/Mushroom JSON data/MushroomDataLoad.swift
+++ ProjectMushroom/Mushroom JSON data/MushroomDataLoad.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct MushroomDataLoad: Decodable {
+    let latin: String
+    let common: [String]
+    let regions: [String]
+    let habitat: String
+    let fairyring: Bool
+    let attributes: ShroomAttributes
+}
+
+
+struct ShroomAttributes: Decodable {
+    let psychoactive: Bool
+    let poisionous: Bool
+    let deadly: Bool
+    let ecology: [String]
+}
