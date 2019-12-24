2cb32460acce4b16b14f4ed28a8b3826abcd3f53
maitreebain <maitreebain@gmail.com>
2019-12-24 01:04:01 -0500

    still working on common names label

--- ProjectMushroom/Edible Mushroom VCs/EdibleDetailController.swift
+++ ProjectMushroom/Edible Mushroom VCs/EdibleDetailController.swift
@@ -29,6 +29,7 @@ class EdibleDetailController: UIViewController {
     func updateUI() {
         
         var counter = 0
+        var index = 0
         
         guard let mushroomInfo = mushroom else {
             print("no mushroomInfo found")
@@ -40,18 +41,22 @@ class EdibleDetailController: UIViewController {
         //mushroomInfo.common.first ?
         
         for info in mushroomInfo.common {
-            if info == mushroomInfo.common.first{
+            if info == mushroomInfo.common[index]{
             commonNameLabel.text = "Common names: \n\(info)"
             print(info)
             counter += 1
+            index += 1
             }
             if counter > 1 {
+
+                mushroomInfo.common[index]
                 commonNameLabel.text = "\n\(info)"
                 print(info)
             }
             print(mushroomInfo.common)
             print(info)
         }
+        
         for regions in mushroomInfo.regions{
         regionsLabel.text = "Found in regions: \n\(regions)"
         }
