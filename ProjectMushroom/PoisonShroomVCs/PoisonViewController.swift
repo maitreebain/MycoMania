c66fb506ccfb7ec19fe43cc4f5e131be662877cb
maitreebain <maitreebain@gmail.com>
2020-01-03 22:51:03 -0500

    added poison shroom vcs and cell, work on custom cell ui

---
+++ ProjectMushroom/PoisonShroomVCs/PoisonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PoisonViewController.swift
+//  ProjectMushroom
+//
+//  Created by Maitree Bain on 1/3/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class PoisonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
