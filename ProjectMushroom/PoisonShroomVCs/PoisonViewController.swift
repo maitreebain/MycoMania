3c5adbfd762e077c9a47c30188cf4a4cb9b971e7
maitreebain <maitreebain@gmail.com>
2020-01-04 23:18:47 -0500

    working on poison table views

--- ProjectMushroom/PoisonShroomVCs/PoisonViewController.swift
+++ ProjectMushroom/PoisonShroomVCs/PoisonViewController.swift
@@ -9,22 +9,31 @@
 import UIKit
 
 class PoisonViewController: UIViewController {
-
+    
+    @IBOutlet weak var poisonTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        poisonTableView.dataSource = self
+        poisonTableView.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PoisonViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+    }
+    
+    
 }
