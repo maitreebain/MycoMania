463ab7d11705b2e91aa2cbe9f9e221bf045bf6e8
maitreebain <maitreebain@gmail.com>
2020-01-05 22:05:00 -0500

    poison view controller table view loaded, work on loading data and custom cell

--- ProjectMushroom/PoisonShroomVCs/PoisonViewController.swift
+++ ProjectMushroom/PoisonShroomVCs/PoisonViewController.swift
@@ -12,6 +12,14 @@ class PoisonViewController: UIViewController {
     
     @IBOutlet weak var poisonTableView: UITableView!
     
+    var mushroom = [MushroomDataLoad]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.poisonTableView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,15 +32,23 @@ class PoisonViewController: UIViewController {
 
 extension PoisonViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return mushroom.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "poisonCell", for: indexPath) as? PoisonCell else {
+            fatalError("poisonCell not conformed")
+        }
+        
+        
+        //configure cell
+        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
+       
+        return 160
     }
     
     
