d1845a0c69f838d29d4154410869fdfbbdd22d9d
maitreebain <maitreebain@gmail.com>
2020-01-08 23:30:37 -0500

    working on mapkit code

---
+++ ProjectMushroom/MapKit/MushroomMaps.swift
@@ -0,0 +1,18 @@
+//
+//  MushroomMaps.swift
+//  ProjectMushroom
+//
+//  Created by Maitree Bain on 1/8/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class MushroomMaps: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//add button to segue into maps, and add mapKit
+}
