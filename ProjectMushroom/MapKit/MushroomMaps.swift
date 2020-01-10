6125eee3dcffbf2eac44ab14f66257b4825a9410
maitreebain <maitreebain@gmail.com>
2020-01-09 23:36:46 -0500

    ui updated

--- ProjectMushroom/MapKit/MushroomMaps.swift
+++ ProjectMushroom/MapKit/MushroomMaps.swift
@@ -26,7 +26,7 @@ class MushroomMaps: UIViewController {
                 continentSegment.setTitle(continents[0], forSegmentAt: 0)
 
                 if mushroomData?.regions.contains("Asia") ?? false{
-                let region = MKCoordinateRegion(center: locations["Asia"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8))
+                let region = MKCoordinateRegion(center: locations["Asia"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60))
                     self.mapView.setRegion(region, animated: true)} else {
                     self.showAlert(title: "Error 4🍄4:", message: "Shroom cannot be found in this area!")
                 }
@@ -34,7 +34,7 @@ class MushroomMaps: UIViewController {
                 continentSegment.setTitle(continents[1], forSegmentAt: 1)
                 
                 if mushroomData?.regions.contains("Europe") ?? false{
-                let region = MKCoordinateRegion(center: locations["Europe"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8))
+                let region = MKCoordinateRegion(center: locations["Europe"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60))
                     self.mapView.setRegion(region, animated: true)
                 } else {
                     self.showAlert(title: "Error 4🍄4:", message: "Shroom cannot be found on this map!")
@@ -43,7 +43,7 @@ class MushroomMaps: UIViewController {
                 continentSegment.setTitle(continents[2], forSegmentAt: 2)
                 
                 if mushroomData?.regions.contains("Australia") ?? false{
-                let region = MKCoordinateRegion(center: locations["Australia"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8))
+                let region = MKCoordinateRegion(center: locations["Australia"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60))
                 self.mapView.setRegion(region, animated: true)
                 } else {
                     self.showAlert(title: "Error 4🍄4:", message: "Shroom cannot be found on this map!")
@@ -52,7 +52,7 @@ class MushroomMaps: UIViewController {
                 continentSegment.setTitle(continents[3], forSegmentAt: 3)
 
                 if mushroomData?.regions.contains("Africa") ?? false {
-                let region = MKCoordinateRegion(center: locations["Africa"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8))
+                let region = MKCoordinateRegion(center: locations["Africa"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60))
                     self.mapView.setRegion(region, animated: true)} else{
                     self.showAlert(title: "Error 4🍄4:", message: "Shroom cannot be found on this map!")
                 }
@@ -60,7 +60,7 @@ class MushroomMaps: UIViewController {
                 continentSegment.setTitle(continents[4], forSegmentAt: 4)
 
                 if mushroomData?.regions.contains("North America") ?? false {
-                    let region = MKCoordinateRegion(center: locations["North America"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8))
+                    let region = MKCoordinateRegion(center: locations["North America"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60))
                     self.mapView.setRegion(region, animated: true)
                 } else {
                     self.showAlert(title: "Error 4🍄4:", message: "Shroom cannot be found on this map!")
@@ -69,14 +69,14 @@ class MushroomMaps: UIViewController {
                 continentSegment.setTitle(continents[5], forSegmentAt: 5)
                 
                 if mushroomData?.regions.contains("South America") ?? false{
-                let region = MKCoordinateRegion(center: locations["South America"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8))
+                let region = MKCoordinateRegion(center: locations["South America"] ?? locations["North America"]!, span: MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60))
                     self.mapView.setRegion(region, animated: true)} else {
                         self.showAlert(title: "Error 4🍄4:", message: "Shroom cannot be found on this map!")
                     }
             default:
                 continentSegment.setTitle(continents[4], forSegmentAt: 4)
                 
-                let region = MKCoordinateRegion(center: locations["North America"] ?? locations["South America"]!, span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8))
+                let region = MKCoordinateRegion(center: locations["North America"] ?? locations["South America"]!, span: MKCoordinateSpan(latitudeDelta: 60, longitudeDelta: 60))
 
                 self.mapView.setRegion(region, animated: true)
             }
@@ -84,7 +84,7 @@ class MushroomMaps: UIViewController {
     }
     
     var locations: [String: CLLocationCoordinate2D] =
-        ["North America": CLLocationCoordinate2D(latitude: 54.5260, longitude: 105.2551),
+        ["North America": CLLocationCoordinate2D(latitude: 37.0902, longitude: 95.7129),
                                                        "Asia": CLLocationCoordinate2D(latitude: 34.0479, longitude: 100.6197),
                                                        "Europe": CLLocationCoordinate2D(latitude: 54.5260, longitude: 15.2551),
                                                        "Australia": CLLocationCoordinate2D(latitude: 25.2744, longitude: 133.7751),
